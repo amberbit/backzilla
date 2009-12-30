7c309c72e1a7f4938fd4a6c5e252020f3d4ff739
Wojciech Piekutowski <wojciech@piekutowski.net>
2009-12-30 17:04:40 +0100

    Directory entity works

--- lib/backzilla/entity/directory.rb
+++ lib/backzilla/entity/directory.rb
@@ -1,11 +1,19 @@
 class Backzilla::Entity::Directory < Backzilla::Entity
   def initialize(name, options)
     super(name)
+    @path = options['path']
   end
 
   def backup
     backup_msg
-    raise 'Not implemented'
+    if @path.blank?
+      fatal 'path option missing'
+      exit -1
+    end
+
+    Backzilla.store @path, project.name, self.name
+
+    @path
   end
 
   def restore
