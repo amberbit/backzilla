6d7675b8d08a8e4ace9e0f0cafb9712f1071c7fc
Wojciech Piekutowski <wojciech@piekutowski.net>
2010-05-20 12:55:15 +0200

    Code cleanup. Make some methods private. #1071

--- lib/backzilla/store/ssh.rb
+++ lib/backzilla/store/ssh.rb
@@ -2,23 +2,24 @@ require 'net/ssh'
 require 'net/sftp'
 
 class Backzilla::Store::SSH < Backzilla::Store
-    
   def initialize(name, options)
     super(name)
     @path = options['path']
     @host = options['host']
     @user = options['user']
   end
-  
-  def protocol
-    'ssh'
-  end
-  
+
   def put(source_path, project_name, entity_name)
     Net::SSH.start(@host, @user ) do |ssh|
-      ssh.exec "mkdir -p " + @path.to_s + "#{project_name}/#{entity_name}"      
+      ssh.exec "mkdir -p " + @path.to_s + "#{project_name}/#{entity_name}"
     end
     super
+  end
+
+  private
+
+  def protocol
+    'ssh'
   end
 
   def uri
@@ -26,7 +27,7 @@ class Backzilla::Store::SSH < Backzilla::Store
       fatal "Missing path option"
       exit -1
     end
-    
+
     uri = ''
     uri << "#{@user}@" unless @user.blank?
     uri << @host
